b74b3522bd4468b0cb47aaf198e79c34d12f6847
Angel Chou <cat77120377@hotmail.com>
2019-04-16 09:23:01 +0800

    Initial Commit

---
+++ OrderDrinksApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrderDrinksApp
+//
+//  Created by Shun-Ching, Chou on 2019/4/16.
+//  Copyright © 2019 Shun-Ching, Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
